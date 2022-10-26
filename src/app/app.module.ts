54fa5b075364cdb03f635b695a2590738891188d
Will Gunter <wgunter@udel.edu>
2022-10-26 15:06:46 -0400

    Completion of in-class activity 4 - setup angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
