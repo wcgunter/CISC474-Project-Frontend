9ca1173e25ab2e529f38ef11c2423175d9fc03ec
Robert Sammataro <robbysam@udel.edu>
2022-11-09 14:48:09 -0500

    Add admin page component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { AdminPageComponent } from './admin-page/admin-page.component';
 import { HomeComponent } from './home/home.component';
 import { LoginFormComponent } from './login-form/login-form.component';
 
@@ -9,6 +10,7 @@ const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'about', component: AboutComponent},
   {path: 'login', component: LoginFormComponent},
+  {path: 'admin', component: AdminPageComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { ToolbarComponent } from './toolbar/toolbar.component';
 //All material components
 import { MaterialModule } from './material/material.module';
 import { LoginFormComponent } from './login-form/login-form.component';
+import { AdminPageComponent } from './admin-page/admin-page.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { LoginFormComponent } from './login-form/login-form.component';
     HomeComponent,
     AboutComponent,
     ToolbarComponent,
-    LoginFormComponent
+    LoginFormComponent,
+    AdminPageComponent
   ],
   imports: [
     BrowserModule,
